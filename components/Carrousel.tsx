cf4aa5373d8afa0eaf66372a7ec23dbcf4abc484
Tiphaine <tiphaine.juquel@gmail.com>
2023-11-14 16:47:22 +0100

    [Component] - Update file

--- components/Carrousel.tsx
+++ components/Carrousel.tsx
@@ -3,11 +3,28 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function Carrousel(){
     return(
         <View>
-
+            <View style={styles.position}>
+                <Text style={styles.title}>Populaires</Text>
+                <Text style={styles.subtitle}>Nouveaux</Text>
+                <Text style={styles.subtitle}>Recommandés</Text>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    position: {
+      display: 'flex',
+      flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    title: {
+        fontSize: 13,
+        fontWeight: 'bold',
+        color: '#fff'
+    },
+    subtitle: {
+        fontSize: 13,
+        color: '#7B8395',
+    }
 })

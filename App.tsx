cee42f6e8977b9310b932445dff853f86ba3e1a8
Tiphaine <tiphaine.juquel@gmail.com>
2023-11-14 16:46:26 +0100

    [App] - Update

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Image} from 'react-native';
+import { StyleSheet, View} from 'react-native';
 
 import Home from './pages/Home';
 

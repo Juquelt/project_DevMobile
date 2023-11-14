f255ea586d5b3f73eb9d0ca338dd6b22306c7051
Tiphaine <tiphaine.juquel@gmail.com>
2023-11-14 15:14:42 +0100

    [Component] - Update file Profil

--- components/Profil.tsx
+++ components/Profil.tsx
@@ -3,11 +3,12 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function Profil() {
     return (
     <View style={styles.profil}>
-
-        <Image
-            source={require('../assets/home/imageProfil.png')}
-            style={styles.imageProfil}
-        />
+        <View style={styles.circleProfil}>
+            <Image
+                source={require('../assets/home/imageProfil.png')}
+                style={styles.imageProfil}
+            />
+        </View>
 
         <View style={styles.text}>
             <Text style={styles.new}>Nouveau</Text>
@@ -15,32 +16,44 @@ export default function Profil() {
             <View style={styles.line}>
                 <Text style={styles.name}>Hey, Lucien !</Text>
                 <Image
-                    source={require('../assets/home/Vector.png')}
+                    source={require('../assets/home/mousse.png')}
                     style={styles.imageVector}
                 />
             </View>
         </View>
-
     </View>
+
     )
 }
 
 const styles = StyleSheet.create({
     profil: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        flexDirection: "row",
 
     },
+    circleProfil: {
+        borderRadius: 50,
+        backgroundColor: '#4B87FF',
+        width: 48,
+        height: 48,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     imageProfil: {
-        width: 100,
-        height: 100,
+        width: 42,
+        height: 42,
         borderRadius: 50,
     },
     text: {
-        backgroundColor: '#fff'
+        display: 'flex',
+        justifyContent: 'center',
+        marginLeft: 5,
     },
     new: {
       textDecorationColor: '#7B8395',
-        fontFamily: 'Open Sans',
-
+        color: '#7B8395',
     },
     line: {
         display: "flex",
@@ -49,13 +62,16 @@ const styles = StyleSheet.create({
     name: {
 
         paddingLeft: 10,
-        fontSize: 13
+        fontSize: 13,
+        fontWeight: 'bold',
+        color: '#fff',
     },
     imageVector: {
       marginTop: 3,
         marginLeft: 3,
         width: 8,
         height: 12,
+        color: '#7B8395',
     },
 
 

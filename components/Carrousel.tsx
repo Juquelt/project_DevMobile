c2f3137ad8088321c905efd2242db481e015da76
Tiphaine <tiphaine.juquel@gmail.com>
2023-11-14 16:29:42 +0100

    [Component] - Add a new file Carrousel.tsx

---
+++ components/Carrousel.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function Carrousel(){
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
